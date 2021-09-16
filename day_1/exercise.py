994b1d560c40980dae2a50cc998ad06babf6c73b
Leo Lai <89428900+Iyaoyas169@users.noreply.github.com>
2021-09-15 20:04:51 -0700

    9/15/2021 Changes

--- day_1/exercise.py
+++ day_1/exercise.py
@@ -5,36 +5,92 @@
 # List of Asset on one sheet
 # Metrics on second sheet
 
+# ITP Week 4 Day 1 Exercise
+
+# https://data.messari.io/api/v2/assets
+
+
 
 import requests
 import json
 import openpyxl
-
-# wb = openpyxl.load_workbook("C:\\Users\Leo.Lai.COMPUSOFT\OneDrive - Compusoft AS\Documents\GitHub\itp_week_4\day_1\output.xlsx")
-# sheet = wb["Sheet1"]
-
-def get_data(url):
-    response = requests.get(url)
-    # print(response)
-    json_result = response.text
-    # print(json_result)
-    # print(type(json_result))
-    clean_data = json.loads(json_result)
-    result = clean_data["data"]
-    return result
-
-def retreive_symbol(list):
-    new_list = []
-    for each in list:
-        new_list.append(each['symbol'])
-    return new_list
+from openpyxl.styles import Font
 
 
-asset_list = get_data("https://data.messari.io/api/v1/assets")
-# print(asset_list)
-symbol_list = retreive_symbol(asset_list)
-print(symbol_list)
-#write_data(result_1)
+data = requests.get('https://data.messari.io/api/v2/assets')
+beautify = json.loads(data.text)
+
+symbol = beautify['data']
+
+roi = beautify['data'][0]['metrics']['roi_data']['percent_change_last_1_week'] 
+
+# print(roi)
+# print(symbol)
+
+
+wb = openpyxl.load_workbook('C:\\Users\\GorkhaliSquad\\Documents\\VetsInTech\\itp_week_4\\day_1\\output.xlsx')
+
+sheet = wb['Sheet1']
+
+sheet['A1'] = "Symbol"
+sheet['A1'].font = Font(bold = True)
+sheet['B1'] =  'ROI'
+sheet['B1'].font = Font(bold = True)
+
+sym_counter = 0
+sheet_counter = 2
+
+for index in range(20):
+    symbol_list = beautify['data'][sym_counter]['symbol']
+    sheet['A' + str(sheet_counter)] = symbol_list
+    sheet_counter += 1
+    sym_counter += 1
+
+
+sheet2_counter = 2
+roi_counter = 0
+
+for item in range(20):
+    roi_list = beautify['data'][roi_counter]['metrics']['roi_data']['percent_change_last_1_week'] 
+    sheet['B' + str(sheet2_counter)] = roi_list
+    sheet2_counter += 1
+    roi_counter += 1
+
+
+
+
+
+wb.save('C:\\Users\\GorkhaliSquad\\Documents\\VetsInTech\\itp_week_4\\day_1\\output.xlsx')
+
+# import requests
+# import json
+# import openpyxl
+
+# # wb = openpyxl.load_workbook("C:\\Users\Leo.Lai.COMPUSOFT\OneDrive - Compusoft AS\Documents\GitHub\itp_week_4\day_1\output.xlsx")
+# # sheet = wb["Sheet1"]
+
+# def get_data(url):
+#     response = requests.get(url)
+#     # print(response)
+#     json_result = response.text
+#     # print(json_result)
+#     # print(type(json_result))
+#     clean_data = json.loads(json_result)
+#     result = clean_data["data"]
+#     return result
+
+# def retreive_symbol(list):
+#     new_list = []
+#     for each in list:
+#         new_list.append(each['symbol'])
+#     return new_list
+
+
+# asset_list = get_data("https://data.messari.io/api/v1/assets")
+# # print(asset_list)
+# symbol_list = retreive_symbol(asset_list)
+# print(symbol_list)
+# #write_data(result_1)
 
 
 
--- day_3/lecture.py
+++ day_3/lecture.py
@@ -1,1 +1,55 @@
 # ITP Week 4 Day 3 Lecture
+
+# imports up top
+import requests
+import json
+import openpyxl
+import functions
+
+pokemon_list = []
+
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+# sequence of commands
+functions.make_header(sheet)
+functions.populate_pokemon(requests, json, "https://pokeapi.co/api/v2/pokemon/", pokemon_list)
+functions.populate_data(requests, json, pokemon_list)
+wb.save("/home/dkayzee/vit/intro-python-august-2021/itp_week_4/day_3/output.xlsx")
+
+def populate_pokemon(requests, json, url, pokemon_list):
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    for each in json_data['results']:
+        pokemon_list.append(each)
+    if json_data['next'] != None:
+        populate_pokemon(json_data['next'])
+
+
+def make_header(sheet):
+    sheet['A1'] = "Name"
+    sheet['B1'] = "Abilities"
+    # make them bold
+
+def write_rows(sheet, row_num, name, abilities):
+    sheet['A' + str(row_num)] = name
+    sheet['B' + str(row_num)] = abilities
+
+
+def retrieve_abilities(requests, json, url):
+    abil_resp = requests.get(url)
+    abil_json_data = json.loads(abil_resp.text)
+    return abil_json_data['abilities']
+
+def populate_data(requests, json, pokemons):
+    for (row_num, each_pokemon) in enumerate(pokemons, start=2):
+        just_abil_list = retrieve_abilities(requests, json, each_pokemon['url'])
+        abil_string = stringify_abilities(just_abil_list)
+        write_rows(row_num, each_pokemon['name'], abil_string)
+
+
+def stringify_abilities(abilities_list):
+    result_string = ""
+    for ability in abilities_list:
+        result_string += ability['ability']['name'] + " "
+    return result_string
